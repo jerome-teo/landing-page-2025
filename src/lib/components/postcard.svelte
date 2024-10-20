c57486ff52d888c5328324c5da605dd18e1f8239
Jeromeoui <zherong.teo@gmail.com>
2024-10-20 13:00:43 -0700

    Update postcard.svelte

--- src/lib/components/postcard.svelte
+++ src/lib/components/postcard.svelte
@@ -174,13 +174,14 @@
 										Submit
 									</button>
 								{:else}
-									<button
+									<!-- <button
 										disabled
 										type="submit"
 										id="submit"
 										class="px-2 py-2"
 										style="background:#fbf8f2"
-									/>
+									/> -->
+									<div class="lds-ripple"></div>
 								{/if}
 							</div>
 						{:else if success}
@@ -606,5 +607,58 @@
 
 	.checkbox-input:hover {
 		border-color: #2e008b; /* Darker shade on hover */
+	}
+
+	/* loading spinner */
+
+	.lds-ripple,
+	.lds-ripple div {
+		box-sizing: border-box;
+	}
+	.lds-ripple {
+		display: inline-block;
+		position: relative;
+		width: 80px;
+		height: 80px;
+	}
+	.lds-ripple div {
+		position: absolute;
+		border: 4px solid currentColor;
+		opacity: 1;
+		border-radius: 50%;
+		animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+	}
+	.lds-ripple div:nth-child(2) {
+		animation-delay: -0.5s;
+	}
+	@keyframes lds-ripple {
+		0% {
+			top: 36px;
+			left: 36px;
+			width: 8px;
+			height: 8px;
+			opacity: 0;
+		}
+		4.9% {
+			top: 36px;
+			left: 36px;
+			width: 8px;
+			height: 8px;
+			opacity: 0;
+		}
+		5% {
+			top: 36px;
+			left: 36px;
+			width: 8px;
+			height: 8px;
+			opacity: 1;
+		}
+		100% {
+			top: 0;
+			left: 0;
+			width: 80px;
+			height: 80px;
+			opacity: 0;
+		}
 	}
 </style>
