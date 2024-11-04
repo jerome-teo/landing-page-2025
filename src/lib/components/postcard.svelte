1cb54a70b5d2a538d75b2a97d113768c9d83f1aa
Jeromeoui <zherong.teo@gmail.com>
2024-11-04 15:15:01 -0800

    Update postcard.svelte

--- src/lib/components/postcard.svelte
+++ src/lib/components/postcard.svelte
@@ -117,10 +117,11 @@
 	// Step 3: Reactive variable to hold the random name
 	let placeholderName = getRandomName();
 	let placeholderEmail = getRandomEmail();
+	datetime = new Date().toDateString();
+
 	async function submitHandler(e: Event) {
 		console.log('Submitted');
 		loading = true;
-		datetime = new Date().toDateString();
 
 		try {
 			const resp = await fetch('/api/email', {
