eb6d1005427de265340f37da883205bbbf750b9b
Jeromeoui <zherong.teo@gmail.com>
2024-11-08 21:53:25 -0800

    Update +server.ts

--- src/routes/api/email/+server.ts
+++ src/routes/api/email/+server.ts
@@ -22,8 +22,7 @@ export async function POST({ request }: RequestEvent): Promise<Response> {
           userId,
           name,
           college,
-          needsTravelStipend,
-          datetime,
+          needsTravelStipend
         })
       }
     );
